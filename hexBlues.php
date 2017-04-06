11d6ee9bb6ccbce2d881209749b02576b90eba80
Mike Douglas <mikeydouglas@users.noreply.github.com>
2017-04-06 14:27:15 -0400

    Blues 0000ff -> ffffff
    Once again ****FF  where * is wildcard and FF is fixed.

---
+++ hexBlues.php
@@ -0,0 +1,60 @@
+<html>
+
+<head>
+
+    <link href="/assets/css/bootstrap.css" type="text/css" rel="stylesheet" />
+    <script src="/assets/js/jquery-2.1.1.min.js" type="text/javascript"></script>
+    <script src="/assets/js/utils.js" type="text/javascript"></script>
+    <script src="/assets/js/bootstrap.js" type="text/javascript"></script>
+
+<style>
+	body {
+		background-color:#dddddd;
+		padding:20px;
+	}
+	.chip_box{
+		width:50px;
+		height:20px;
+		border:1px solid #333333;
+		display: inline-block;
+		text-shadow: .5px .5px #cccccc;
+		font-size:9px !important;
+		padding:3px;
+	}
+	.chip_box:hover{
+		border-color:#ffffff;
+	}
+
+    </style>
+
+
+</head>
+
+<?php 
+	// If there is not a base color to gradient out set one up  / otherwise permit one to be passed via get
+	if(!isset($_GET['base_color'])) 	$base_color = "#ffffff";
+	else 											$base_color = $_GET['base_color'];
+
+	$max_chips = 1000;			// this will be the number of itterations in the color variation process
+	$max_num  = 65535;	// This is FFFFFF pure white in HEX and RGB where RRGGBB are the placeholders respectivelt
+	$multiplier = intval($max_num/$max_chips);
+?>
+
+<body>
+
+	All colours (blues) evenly parsed out in (1000) equal segments <br>
+	
+<div class="row">
+
+<?php
+	
+	for ($i=1; $i <= $max_chips; $i++){
+		
+		$tColor = dechex( $i * $multiplier);
+		$tColor = str_pad($tColor , 4, "0", STR_PAD_LEFT);
+		echo "<div  class=\"chip_box\" style=\"background-color:#".$tColor."FF".$tColor2."\">".$tColor."FF</div>\n";
+		
+	}
+?>
+
+</div>
